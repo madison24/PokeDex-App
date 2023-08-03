e73de6c2bee438602c4bfc2a4ec0d2409f37f2bd
madison24 <mhousman24@gmail.com>
2023-08-03 13:34:07 -0500

    Added pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert("Hello world");
-let favoriteFood = "Pasta";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+  { name: "Butterfree", height: 5, types: ["bug", "flying"] },
+  { name: "Slowbro", height: 8, types: ["water", "psychic"] },
+];
