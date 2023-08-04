3216ceca5a8a919a2cba52fd1d908e6f96dda53f
madison24 <mhousman24@gmail.com>
2023-08-03 19:29:37 -0500

    Updated js
    Added a list with pokemon height next to name and another list with a sentence next to the tallest item.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-  { name: "Butterfree", height: 5, types: ["bug", "flying"] },
-  { name: "Slowbro", height: 8, types: ["water", "psychic"] },
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Butterfree", height: 1.2, types: ["bug", "flying"] },
+  { name: "Slowbro", height: 0.8, types: ["water", "psychic"] },
 ];
+
+// The list of pokemon with height next to the name //
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+    " " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") "
+  );
+}
+
+//The list of pokemon with text next to the tallest//
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        " - Wow, that's big!"
+    );
+  } else {
+    document.write(
+      " " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") "
+    );
+  }
+}
