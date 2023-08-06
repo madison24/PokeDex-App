7dabcf010e3cdeb63a19c8f9e31a28cd7e2390ba
madison24 <mhousman24@gmail.com>
2023-08-06 13:39:26 -0500

    updated for loop to for each

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,34 @@ let pokemonList = [
   { name: "Slowbro", height: 0.8, types: ["water", "psychic"] },
 ];
 
-// The list of pokemon with height next to the name //
-
-for (let i = 0; i < pokemonList.length; i++) {
+pokemonList.forEach(function (pokemon) {
   document.write(
-    " " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") "
+    "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>"
   );
-}
+});
 
 //The list of pokemon with text next to the tallest//
 
-for (let i = 0; i < pokemonList.length; i++) {
+/* for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 1) {
     document.write(
-      pokemonList[i].name +
+      "<p>" +
+        pokemonList[i].name +
         " (height: " +
         pokemonList[i].height +
         ")" +
+        " " +
         " - Wow, that's big!"
     );
   } else {
     document.write(
-      " " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") "
+      "<p>" +
+        pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        "</p>"
     );
   }
 }
+*/
