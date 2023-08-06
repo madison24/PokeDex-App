a3bff92583190b45ed44b34e00c16135e697b999
madison24 <mhousman24@gmail.com>
2023-08-06 14:19:41 -0500

    Updated JS with IIFE
    I added an IIFE around the pokemon.List

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Butterfree", height: 1.2, types: ["bug", "flying"] },
-  { name: "Slowbro", height: 0.8, types: ["water", "psychic"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Butterfree", height: 1.2, types: ["bug", "flying"] },
+    { name: "Slowbro", height: 0.8, types: ["water", "psychic"] },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
 
 pokemonList.forEach(function (pokemon) {
   document.write(
